d4b37d5b676ada655d884394ff9d7f90fffba8a3
kkuikkya <kkuikkya@gmail.com>
2025-08-19 13:42:53 +0000

    sumSubset

--- sumSubset.py
+++ sumSubset.py
@@ -1,4 +1,6 @@
-def sumSubset(setList, X, memo):
+from collections import deque
+
+def sumSubset(setList, X):
     """하향식 재귀 호출로 해결"""
 
     #base case
@@ -13,8 +15,6 @@ def sumSubset(setList, X, memo):
 
     return sumSubset(remainList, X-p) or sumSubset(remainList, X)
 
-
-print(sumSubset([1,2,3,6],9))
 
 def sumSubsetDP(setList,X):
 
@@ -45,4 +45,104 @@ def sumSubsetDP(setList,X):
 
             memo[(i,j)] = include or exclude
 
+
     return memo[(n, X)]
+
+
+
+def sumSubsetDPPrint(setList,X):
+    """배열 출력하기"""
+
+    memo = {} # memo[(i,j)] 원소 i 개로 합 j 를 만들수 있는가? (j <= X)
+
+    memo[(0,0)] = True
+
+    n = len(setList)
+
+    for i in range(1, n + 1):
+        memo[(i, 0)] = True
+
+
+    for j in range(1, X + 1):
+        memo[(0, j)] = False
+
+    for i in range(1,n+1):
+        num = setList[i-1] # 현재 보고 있는 숫자
+
+        for j in range(1,X+1):
+            
+            include = False
+            if j >= num:
+                include = memo[(i-1, j-num)]
+
+            exclude = memo[(i-1, j)]
+
+
+            memo[(i,j)] = include or exclude
+
+
+    result = []
+    
+    # 백트래킹 DFS
+    def findAll(i, j, current_subset):
+        if j == 0:
+            result.append(current_subset)
+            return
+
+        if i == 0:
+            return
+        
+        num = setList[i-1]
+
+        # exclude
+        if memo.get((i - 1, j), False):
+            findAll(i - 1, j, current_subset)
+
+        # include
+        if j >= num and memo.get((i - 1, j - num), False):
+            findAll(i - 1, j - num, [num] + current_subset)
+
+    findAll(n, X, [])
+
+    print(result)
+
+    return memo[(n, X)]
+
+
+
+
+def onlyTwoForX(setList, X):
+    """연습문제 5-7"""
+
+# 1. 일단 정렬
+# 2. 원소 선택
+# 3. 남은 원소들 중 가운데 값을 X와 비교
+# 4. 반복..
+
+    tmplist = sorted(setList)
+
+    n = len(tmplist)
+    
+    for i in range(n):
+        num1 = tmplist[i]
+
+        left = i + 1
+        right = n-1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if num1 + tmplist[mid] < X:
+                left = mid + 1
+        
+            elif num1 + tmplist[mid] > X:
+                right = mid - 1
+
+            else:
+                return True
+    
+    return False
+        
+
+
+sumSubsetDPPrint([1,2,3,6], 6)
